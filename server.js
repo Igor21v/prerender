98475b56b124f3e1152347b8a0bbce8d152b3fc8
Igor Bondarenko <igor21v@mail.ru>
2024-01-08 22:21:03 +0800

    fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const prerender = require("prerender");
 const server = prerender();
-// server.use(require("prerender-memory-cache"));
+server.use(require("prerender-memory-cache"));
 server.start();
