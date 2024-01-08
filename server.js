f9e93d6ce25a1876d035276d251b324d3305722a
Igor Bondarenko <igor21v@mail.ru>
2024-01-08 22:28:32 +0800

    feat: change ttl, change maxsize

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const prerender = require("prerender");
 const server = prerender();
 server.use(require("prerender-memory-cache"));
+export const CACHE_MAXSIZE = 1000;
+export const CACHE_TTL = 5184000;
 server.start();
