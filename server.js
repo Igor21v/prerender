e0b32138b24d6e99a71d23377fc392e4f53a3a88
Igor Bondarenko <igor21v@mail.ru>
2024-01-08 22:40:22 +0800

    fix

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const prerender = require("prerender");
 const server = prerender();
 server.use(require("prerender-memory-cache"));
-export const CACHE_MAXSIZE = 1000;
-export const CACHE_TTL = 5184000;
 server.start();
