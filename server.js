11845dd520bdce9f6f20c8f6e2476634c79e62bd
Igor Bondarenko <igor21v@mail.ru>
2024-01-09 13:23:50 +0800

    build: add prerender-node

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const prerender = require("prerender");
 var redis = require("redis"),
   client = redis.createClient();
 const server = prerender();
-server
-  .set("beforeRender", function (req, done) {
-    client.get(req.url, done);
-  })
-  .set("afterRender", function (err, req, prerender_res) {
-    client.set(req.url, prerender_res.body);
-  });
+
+server.use(
+  require("prerender-node")
+    .set("beforeRender", function (req, done) {
+      client.get(req.url, done);
+    })
+    .set("afterRender", function (err, req, prerender_res) {
+      client.set(req.url, prerender_res.body);
+    })
+);
 //server.use(require("prerender-memory-cache"));
 server.start();
