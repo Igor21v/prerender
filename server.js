ed9207914df0adebffe6ed6719a59e73a06e7fa0
Igor Bondarenko <igor21v@mail.ru>
2024-01-09 16:46:40 +0800

    fix: delete libs

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 const prerender = require("prerender");
 const server = prerender();
-
-/* var redis = require("redis"),
-  client = redis.createClient();
-server.use(
-  require("prerender-node")
-    .set("beforeRender", function (req, done) {
-      client.get(req.url, done);
-    })
-    .set("afterRender", function (err, req, prerender_res) {
-      client.set(req.url, prerender_res.body);
-    })
-); */
-
 server.use(require("prerender-level-cache"));
 //server.use(require("prerender-memory-cache"));
 server.start();
