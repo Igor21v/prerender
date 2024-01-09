20ecadf8c93ad9c6fe5964b59f1990d3cf166e4e
Igor Bondarenko <igor21v@mail.ru>
2024-01-09 13:06:07 +0800

    feat: add redis for caching

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const prerender = require("prerender");
+var redis = require("redis"),
+  client = redis.createClient();
+
+prerender
+  .set("beforeRender", function (req, done) {
+    client.get(req.url, done);
+  })
+  .set("afterRender", function (err, req, prerender_res) {
+    client.set(req.url, prerender_res.body);
+  });
 const server = prerender();
-server.use(require("prerender-memory-cache"));
 server.start();
