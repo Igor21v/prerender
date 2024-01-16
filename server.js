2a02031f62c2e8b4c313f8d2939da27e381f4121
Igor Bondarenko <igor21v@mail.ru>
2024-01-16 23:08:32 +0800

    test: setup chrome

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const prerender = require("prerender");
-const server = prerender();
-server.use(require("prerender-level-cache"));
+
+const server = prerender({
+  chromeLocation: `C:/Program Files/Google/Chrome/Application/chrome.exe`,
+  chromeFlags: [
+    "--no-sandbox",
+    "--headless",
+    "--disable-gpu",
+    "--remote-debugging-port=9222",
+    "--hide-scrollbars",
+  ],
+});
+/* server.use(require("prerender-level-cache")); */
 //server.use(require("prerender-memory-cache"));
 server.start();
